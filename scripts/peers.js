a77285a3b09683ae061a0a04962c2ed7b80a5e9f
Rubber-Duckie <53319864+rubber-duckie-au@users.noreply.github.com>
2019-11-21 08:01:30 +1100

    fix for Ipv6 peers and geolocation

--- scripts/peers.js
+++ scripts/peers.js
@@ -26,13 +26,16 @@ mongoose.connect(dbString, function(err) {
     request({uri: 'http://127.0.0.1:' + settings.port + '/api/getpeerinfo', json: true}, function (error, response, body) {
       lib.syncLoop(body.length, function (loop) {
         var i = loop.iteration();
-        var address = body[i].addr.split(':')[0];
+        if (body[i].addr.indexOf("]") > -1) {
+              var temp_address = body[i].addr.split(']')[0]
+              var address = temp_address.replace('[', '')
+        else {var address = body[i].addr.split(':')[0];}
         db.find_peer(address, function(peer) {
           if (peer) {
             // peer already exists
             loop.next();
           } else {
-            request({uri: 'http://freegeoip.net/json/' + address, json: true}, function (error, response, geo) {
+            request({uri: 'http://api.ipstack.com/' + address + '?access_key=' + settings.ipstack_apikey, json: true}, function (error, response, geo) {
               db.create_peer({
                 address: address,
                 protocol: body[i].version,
