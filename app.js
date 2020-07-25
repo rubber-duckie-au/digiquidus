56156f26ae42510f1298109830f51e765d6a4f43
Rubber-Duckie <53319864+rubber-duckie-au@users.noreply.github.com>
2020-07-25 14:07:31 +1000

    Address Labels -> Claim Address

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
 bitcoinapi.setWalletDetails(settings.wallet);
 if (settings.heavy != true) {
   bitcoinapi.setAccess('only', ['getinfo', 'getnetworkhashps', 'getmininginfo','getdifficulty', 'getconnectioncount',
-    'getblockcount', 'getblockhash', 'getblock', 'getrawtransaction', 'getpeerinfo', 'gettxoutsetinfo', 'getrawmempool', 'masternodelist']);
+    'getblockcount', 'getblockhash', 'getblock', 'getrawtransaction', 'getpeerinfo', 'gettxoutsetinfo', 'getrawmempool', 'masternodelist', 'verifymessage']);
 } else {
   // enable additional heavy api calls
   /*
@@ -43,7 +43,7 @@ if (settings.heavy != true) {
   bitcoinapi.setAccess('only', ['getinfo', 'getstakinginfo', 'getnetworkhashps', 'getdifficulty', 'getconnectioncount',
     'getblockcount', 'getblockhash', 'getblock', 'getrawtransaction','getmaxmoney', 'getvote',
     'getmaxvote', 'getphase', 'getreward', 'getnextrewardestimate', 'getnextrewardwhenstr',
-    'getnextrewardwhensec', 'getsupply', 'gettxoutsetinfo', 'getrawmempool', 'masternodelist']);
+    'getnextrewardwhensec', 'getsupply', 'gettxoutsetinfo', 'getrawmempool', 'masternodelist', 'verifymessage']);
 }
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -220,6 +220,28 @@ app.use('/ext/getaddresstxsajax/:address', function(req,res){
         res.json({"data":data, "draw": req.query.draw, "recordsTotal": count, "recordsFiltered": count});
     });
 });
+
+app.post('/address/:hash/claim', function(req, res){
+  var address = req.body.address;
+  var signature = req.body.signature;
+  var message = req.body.message;
+  request({
+      url: 'http://127.0.0.1:' + settings.port + '/api/verifymessage?address='+address+ '&signature='+ signature + '&message=' + message,
+    method: 'GET',
+  }, function(error, response, body){
+    //console.log('error', error);
+    //console.log('response', response);
+    if(body == "false"){
+      console.log('failed');
+      res.json({"status": "failed", "error":true, "message": error});
+    }else if(body == "true"){
+      db.update_label(address, message, function(){
+        res.json({"status": "success"});
+      })
+    }
+  });
+})
+
 
 app.use('/ext/connections', function(req,res){
   db.get_peers(function(peers){
